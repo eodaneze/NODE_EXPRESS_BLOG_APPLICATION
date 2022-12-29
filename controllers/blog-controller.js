dc626e79cb5669b7b1fecbd91043a9bc11c1fc2c
Ezeali Daniel Okechukwu <ezealiokechukwu1999@gmail.com>
2022-12-29 15:13:00 -0800

    gitting blog by thr id of the user

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -102,3 +102,22 @@ export const deleteBlog = async (req, res, next) => {
   }
   return res.status(200).json({ message: "Blog was deleted successfully" });
 };
+
+export const getByUserId = async (req, res, next) => {
+  const userId = req.params.id;
+
+  let userBlogs;
+
+  try{
+    userBlogs = await User.findById(userId).populate("blog");
+  }
+
+  catch (err) {
+    return console.log(err);
+  }
+
+  if(!userBlogs){
+    return res.status(404).json({ message: "No Blog Found" })
+  }
+  return res.status(200).json({ blog: userBlogs });
+};
--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addBlog, deleteBlog, getAllBlogs, getById, updateBlog } from "../controllers/blog-controller.js";
+import { addBlog, deleteBlog, getAllBlogs, getById, getByUserId, updateBlog } from "../controllers/blog-controller.js";
 
 const blogRouter = express.Router();
 
@@ -8,5 +8,6 @@ blogRouter.post("/add", addBlog );
 blogRouter.put("/update/:id", updateBlog);
 blogRouter.get("/:id", getById);
 blogRouter.delete("/:id", deleteBlog);
+blogRouter.get("/user/:id", getByUserId);
 
 export default blogRouter;
