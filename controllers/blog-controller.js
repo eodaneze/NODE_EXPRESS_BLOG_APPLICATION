ed4ce23da8d8ea3bb080ad29aa9892972cab9dec
Ezeali Daniel Okechukwu <ezealiokechukwu1999@gmail.com>
2022-12-29 13:59:51 -0800

    blog delete request

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -63,9 +63,26 @@ export const getById = async (req, res, next) => {
     }
 
     if(!blog){
-        return res.status(200).json({ message: "No Blog Found"});
+        return res.status(400).json({ message: "No Blog Found"});
   
     }
     return res.status(200).json({ blog });
   
+}
+
+export const deleteBlog = async (req, res, next) => {
+    const id = req.params.id;
+
+    let blog;
+
+    try{
+        blog = await Blog.findByIdAndRemove(id)
+     }catch(err){
+          return console.log(err);
+     }
+     if(!blog){
+        return res.status(500).json({ message: "Unable to delete the blog" });
+  
+    }
+    return res.status(200).json({ message: "Blog was deleted successfully"  });
 }
--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addBlog, getAllBlogs, getById, updateBlog } from "../controllers/blog-controller.js";
+import { addBlog, deleteBlog, getAllBlogs, getById, updateBlog } from "../controllers/blog-controller.js";
 
 const blogRouter = express.Router();
 
@@ -7,5 +7,6 @@ blogRouter.get("/", getAllBlogs )
 blogRouter.post("/add", addBlog );
 blogRouter.put("/update/:id", updateBlog);
 blogRouter.get("/:id", getById);
+blogRouter.delete("/:id", deleteBlog);
 
 export default blogRouter;
